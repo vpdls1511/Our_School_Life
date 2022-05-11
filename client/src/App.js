8557ea21d054eb53ea97f1b79a52f4c9ef802c8b
nGyu <vpdls1511@gmail.com>
2022-05-11 17:57:39 +0900

    feat(vpdls1511) : 라우팅 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useRoutes} from 'react-router-dom';
+//Layouts
+import Default from "./layout/Default";
+//Pages
+import Home from "./page/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return useRoutes([
+    {
+      path : '/',
+      element : <Default />,
+      children : [
+        {index : true , element: <Home />}
+      ]
+    }
+  ]);
 }
 
 export default App;
---
+++ client/src/layout/Default.js
@@ -0,0 +1,10 @@
+import {Outlet} from 'react-router-dom'
+
+const Default = () => {
+  return <>
+    Default dafsdfasdfasdfasdfasdfasdf
+    <Outlet />
+  </>
+}
+
+export default Default;
