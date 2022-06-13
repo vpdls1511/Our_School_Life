c742776a4d689f0840f5ca67dc59f6a6d0c07c4a
nGyu <vpdls1511@gmail.com>
2022-06-13 23:18:24 +0900

    test(vpdls1511) : 최근 불만사항 20개 가져오기 테스트

---
+++ server/src/test/java/com/life/school/our/server/application/post/service/PostServiceTest.java
@@ -0,0 +1,22 @@
+package com.life.school.our.server.application.post.service;
+
+import com.life.school.our.server.application.post.repository.GetPostsDataRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private GetPostsDataRepository getPostsDataRepository;
+
+    @Test
+    void 최근_불만사항_가져오기(){
+        assertThat(getPostsDataRepository.getRecentlySuggest(0).size())
+                .isEqualTo(20);
+    }
+}
